ffba0eaa721f75fcb72776188b1bd28ad64a4164
Sai-Khush <sm54858n@pace.edu>
2025-10-08 12:33:14 -0400

    Fix: switched next.config.ts to JS to prevent Jenkins TypeScript error

--- Code/walleto/next.config.ts
+++
@@ -1,8 +0,0 @@
-// next.config.ts
-const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  // optional: ensures proper links like /about/ instead of /about
-  // trailingSlash: true,
-};
-export default nextConfig;
